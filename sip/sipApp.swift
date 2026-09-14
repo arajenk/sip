945851f3dd35dbb93353a0548e41921f527c2e2f
Arajen Kajanthirabalan <arajenkajanthira@gmail.com>
2026-09-13 20:44:43 -0400

    Initial Commit

---
+++ sip/sipApp.swift
@@ -0,0 +1,17 @@
+//
+//  sipApp.swift
+//  sip
+//
+//  Created by Arajen Kajanthirabalan on 2026-09-13.
+//
+
+import SwiftUI
+
+@main
+struct sipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
